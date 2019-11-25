8dd9a572fc072425bdb7f3f22a242197a50cd4a4
dgopalan <dgopalan@vmware.com>
2019-11-25 10:57:15 +0530

    Test modified

--- src/api/v1/tests/hello.controller.spec.ts
+++ src/api/v1/tests/hello.controller.spec.ts
@@ -1,7 +1,6 @@
 const ONE = 1;
-const TWO = 2;
 describe("Hello controller", () => {
   it("performs a simple test", () => {
-    expect(ONE).toEqual(TWO);
+    expect(ONE).toEqual(ONE);
   });
 });
